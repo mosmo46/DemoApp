48d50d69aba904b814ae17e2dcc081e9df053fd2
Moshe Yaso <73312834+mosmo46@users.noreply.github.com>
2022-06-20 22:12:00 +0300

    My updated file

--- Calc/TestCreateFile.cs
+++ Calc/TestCreateFile.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calc
-{
-    internal class TestCreateFile
-    {
-    }
-}
-
+Succeed
