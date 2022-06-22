75e7bb82a0c21b2341f906fe45565050d65dbab2
Moshe Yaso <73312834+mosmo46@users.noreply.github.com>
2022-06-22 15:40:48 +0300

    My updated file

--- Calc/Class1.cs
+++ Calc/Class1.cs
@@ -1,1 +1,12 @@
-Hello Universe! 6/22/2022 12:40:44 PM
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    internal class Class1
+    {
+    }
+}
