3ed4df0cb5f30f60ad144d4bce39a8170e6abd46
Moshe Yaso <73312834+mosmo46@users.noreply.github.com>
2022-06-23 17:07:08 +0300

    My updated file

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -17,7 +17,7 @@ namespace CalculatorApp
         static void Main(string[] args)
         { 
             Console.WriteLine("tmp tmppp t");            
-            Console.WriteLine("This program performs basic math operation on 2 numbers");
+            Console.WriteLine("This program performs basic math operation on 2 numbers"); 
             Console.WriteLine("Please insert the first number");
             Console.WriteLine("moshe test");
 
