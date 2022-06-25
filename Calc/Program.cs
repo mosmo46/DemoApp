306e8259aa9c084b8d68bef672f654161cbe61f4
Moshe Yaso <73312834+mosmo46@users.noreply.github.com>
2022-06-25 14:53:49 +0300

    My updated file

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -20,8 +20,6 @@ namespace CalculatorApp
 
             Console.WriteLine("This program performs basic math operation on 2 numbers");
             Console.WriteLine("Please insert the first number");
-            Console.WriteLine("moshe test");
-            Console.WriteLine("moshe test1");
 
             double firstNumber = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Please insert the second tmp");
