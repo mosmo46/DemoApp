267f1fc00b6118ad23f3582b53b23a2aba2760a2
bani010497@gmail.com <bani010497@gmail.com>
2022-06-27 09:53:18 +0300

    add

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -18,6 +18,7 @@ namespace CalculatorApp
         static void Main(string[] args)
         {
             Console.WriteLine("mulu");
+            Console.WriteLine("simm");
             Console.WriteLine("moshe");
             Console.WriteLine("This program performs basic math operation on 2 numbers");
             Console.WriteLine("Please insert the first number");
