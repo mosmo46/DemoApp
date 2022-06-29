616292a58a9cd6aa551b1804b3f4f6e20085268b
Moshe Yaso <73312834+mosmo46@users.noreply.github.com>
2022-06-29 18:54:57 +0300

    Update Program.cs

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -16,7 +16,7 @@ namespace CalculatorApp
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Lior");
+            Console.WriteLine("jsabdijnasdkjasdoijasndjandsoijasdoiasdoasdasd");
             Console.WriteLine("This program performs basic math operation on 2 numbers");
             Console.WriteLine("Please insert the first number");
 
