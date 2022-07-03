ae53c06f869e899f6bcec87eb1fdda43d42e6ee8
Moshe Yaso <73312834+mosmo46@users.noreply.github.com>
2022-07-03 10:15:55 +0300

    My updated file

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -16,7 +16,7 @@ namespace CalculatorApp
 
         static void Main(string[] args)
         {
-            Console.WriteLine("lior ");
+            Console.WriteLine("mulu ddd");
             Console.WriteLine("This program performs basic math operation on 2 numbers");
             Console.WriteLine("Please insert the first number");
 
