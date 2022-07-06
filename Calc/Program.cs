da7890095f24a76c50365a7100caaa805ddc3720
Moshe Yaso <73312834+mosmo46@users.noreply.github.com>
2022-07-06 17:43:44 +0300

    Update Program.cs

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -16,7 +16,6 @@ namespace CalculatorApp
 
         static void Main(string[] args)
         {
-                        Console.WriteLine("moshe test");
 
             Console.WriteLine("moshe test");
             Console.WriteLine("This program performs basic math operation on 2 numbers");
